9fae8d26ca500361ca49def66083cebb745e3959
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2023-07-02 00:21:28 +0300

    Update AppBottom.jsx

--- src/components/AppBottom.jsx
+++ src/components/AppBottom.jsx
@@ -5,7 +5,7 @@ function AppBottom(){
     return(
             <>
             <div className='AppBottomClass'>
-                <p>Bottom</p>
+                <p>هنا هيقوله اركب اتجاه عين شمس مثلا و انزل عين شمس</p>
             </div>
             </>
     )
