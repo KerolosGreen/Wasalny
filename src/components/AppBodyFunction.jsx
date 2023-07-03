107e3d257d373ec3098e96b1bfd9a2f58bdf54be
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2023-07-03 13:17:32 +0300

    Update AppBodyFunction.jsx

--- src/components/AppBodyFunction.jsx
+++ src/components/AppBodyFunction.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import L5atLkam from './L5atLkam';
 import FanhyMa7ata from './FanhyMa7ata';
 import Ray7AnhyMa7ata from './Ray7AnhyMa7ata';
+import UnderDeveloping from './UnderDeveloping';
 
 
 function AppBodyFunction(){
@@ -12,6 +13,7 @@ function AppBodyFunction(){
         <L5atLkam/>
         <FanhyMa7ata/>
         <Ray7AnhyMa7ata/>
+        <UnderDeveloping/>
     </div>
     </>
     )
