e54e988a7ba744ef0a15a1b31ee7cff866a0f7e4
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2023-07-06 03:30:59 +0300

    Update AppHeader.jsx

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -6,7 +6,7 @@ function AppHeader(){
     return(
             <>
             <div className='AppHeaderClass'>
-                <p>وصلني هيقولك تروح ازاي اي محطة مترو</p>
+                <p>وصلني هيقولك توصل ازاي لأي محطة مترو من اي محطة</p>
                 <img src='https://i.pinimg.com/originals/77/33/9b/77339b381571fa03c8e9b6b6e753fa28.gif' width="300vh"/> 
             </div>
             </>
