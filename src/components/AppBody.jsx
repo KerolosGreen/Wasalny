eecb886c922923b9ec2f5ba6cc731216cad5f5fe
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2023-08-01 12:00:16 +0300

    Update AppBody.jsx

--- src/components/AppBody.jsx
+++ src/components/AppBody.jsx
@@ -11,6 +11,20 @@ function AppBody(){
     const [Stationscount , setStationscount]=useState("");
     return(
         <div className='AppBodyClass'>
+            {/*AD*/}
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9275465709873085"
+     crossorigin="anonymous"></script>
+<!-- Wasalny AD1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9275465709873085"
+     data-ad-slot="2908753533"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+            {/*AD*/}
             <AppHeader/>
             <AppBodyFunction sethowtogonow={sethowtogonow} setStationscount={setStationscount}/>
             <AppBottom howtogonow={howtogonow}  Stationscount={Stationscount} />
