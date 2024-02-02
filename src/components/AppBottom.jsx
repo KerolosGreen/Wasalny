9c815086c19daf665ef34c88724148a272828670
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2024-02-02 21:35:29 +0200

    Update AppBottom.jsx

--- src/components/AppBottom.jsx
+++ src/components/AppBottom.jsx
@@ -6,7 +6,7 @@ function AppBottom(props){
     return(
             <>
             <div className='AppBottomClass'>
-                // The Result Description
+                {// The Result Description}
             {props.Stationscount?<p class="bg-dark rounded px-3 text-light p-2" id='resultid'>عدد المحطات : {props.Stationscount} , متوسط وقت الوصول من {parseInt(props.Stationscount)*3} ل {parseInt(props.Stationscount)*5} دقايق</p>:null}
             <p class="bg-dark rounded px-3 text-light p-2" id='resultid'>{props.howtogonow}</p>
             </div>
