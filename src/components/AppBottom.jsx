b918e5d2458f30bcb0e8682957123f8a019e994d
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2024-02-18 02:25:23 +0200

    Add Notification Function

--- src/components/AppBottom.jsx
+++ src/components/AppBottom.jsx
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom';
 import Stations from './Stations';
 
 function AppBottom(props){
+        //Notify The User With The Details In Notifications Bar
+    function addtonotifications(){
+        if(props.howtogonow){
+            Notification.requestPermission()
+        .then(
+            (permission)=>{
+                if(permission=='granted'){
+                    new Notification(props.howtogonow)
+                }
+            }
+        )
+        }  
+    }
     return(
             <>
             <div className='AppBottomClass'>
             {props.Stationscount?<p class="bg-dark rounded px-3 text-light p-2" id='resultid'>عدد المحطات : {props.Stationscount}
                 , متوسط وقت الوصول من {parseInt(props.Stationscount)*3} ل {parseInt(props.Stationscount)*5} دقايق</p>:null}
             <p class="bg-dark rounded px-3 text-light p-2" id='resultid'>{props.howtogonow}</p>
+                {props.howtogonow?addtonotifications():null}
             </div>
             </>
     )
